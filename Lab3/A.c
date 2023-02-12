70e08517b22579624cb7cd83d3795a6b397c9dff
Tanish32 <110172756+Tanish32@users.noreply.github.com>
2023-02-12 17:38:09 +0530

    Update A.c

--- Lab3/A.c
+++ Lab3/A.c
@@ -28,53 +28,57 @@ Node *append(Node *head, int data) {
     return head;
   }
 }
-void currentSong() {}
-int main() {
-  // initializing doubly linked list
-  int nodesNum = 0;
-  scanf("%d", &nodesNum);
+Node *takeInputs(int nodesNum) {
   int a = 0, b = 0;
   scanf("%d", &a);
-  Node *head = append(NULL, a);
+  Node *firstHead = append(NULL, a);
   for (int i = 1, a = 0; i < nodesNum; i++) {
 
     scanf("%d", &a);
-    append(head, a);
+    append(firstHead, a);
   }
-  Node *currentSong = head;
-  while (b != 5) {
-    scanf("%d", &b);
-    switch (b) {
-    case 1:
-      scanf("%d", &a);
-      append(head, a);
-      break;
-    case 2:
-      printf("Current song: %d\n", currentSong->data);
-      break;
-    case 3:
-      if (currentSong->next != NULL) {
-        currentSong = currentSong->next;
+  return firstHead;
+}
+int main() {
+  //Initializing array
+  int nodesNum = 0, a = 0, b = 0;
+  scanf("%d", &nodesNum);
+  Node *head = takeInputs(nodesNum);
+    //Quesiton 1 logic
+    Node *currentSong = head;
+    while (b != 5) {
+      scanf("%d", &b);
+      switch (b) {
+      case 1:
+        scanf("%d", &a);
+        append(head, a);
+        break;
+      case 2:
+        printf("Current song: %d\n", currentSong->data);
+        break;
+      case 3:
+        if (currentSong->next != NULL) {
+          currentSong = currentSong->next;
+        }
+        break;
+      case 4:
+        if (currentSong != head) {
+          currentSong = currentSong->prev;
+        }
+        break;
+      case 5:
+        b = 5;
+        break;
+      default:
+        b = 5;
+        printf("Invalid choice");
+        break;
       }
-      break;
-    case 4:
-      if (currentSong != head) {
-        currentSong = currentSong->prev;
-      }
-      break;
-    case 5:
-      b = 5;
-      break;
-    default:
-      b = 5;
-      printf("Invalid choice");
-      break;
     }
+  //   To print the array
+  Node *ptr = head;
+  for (int i = 0; i < nodesNum; i++) {
+    printf("%d ", ptr->data);
+    ptr = ptr->next;
   }
-  // To print the array
-  // Node *ptr = head;
-  // for (int i = 0; i < nodesNum; i++) {
-  //   printf("%d ", ptr->data);
-  //   ptr = ptr->next;
-  // }
 }
