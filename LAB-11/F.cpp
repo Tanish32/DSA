2e11d8824a5d75b768d9685428acbb676a671d06
Tanish32 <110172756+Tanish32@users.noreply.github.com>
2023-05-17 17:54:09 +0530

    find num of values from array to reach given sum

--- LAB-11/F.cpp
+++ LAB-11/F.cpp
@@ -1,7 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
-int couponChange(vector<int> v, int k)
+int couponChange(vector<int> v, int k, unordered_map<int, int> mp)
 {
+    if(mp[k]!=0){
+        return mp[k];
+    }
     if (k < 0)
     {
         return INT_MAX;
@@ -14,17 +17,19 @@ int couponChange(vector<int> v, int k)
     int min = INT_MAX;
     for (int i = 0; i < v.size(); i++)
     {
-        returns[i] = couponChange(v, k - v[i]);
+        returns[i] = couponChange(v, k - v[i], mp);
         if (returns[i] < min)
             min = returns[i];
     }
+    
     if (min == INT_MAX)
     {
         return INT_MAX;
     }
     else
     {
-        return min + 1;
+        mp[k] = min+1;
+        return mp[k];
     }
 }
 int main()
@@ -32,13 +37,14 @@ int main()
     int n, k, min = INT_MAX;
     cin >> n >> k;
     vector<int> v(n);
+    unordered_map<int, int> mp;
     for (int i = 0; i < n; i++)
     {
         cin >> v[i];
         if (v[i] < min)
             min == v[i];
     }
-    int val = couponChange(v, k);
+    int val = couponChange(v, k, mp);
     if (val == INT_MAX)
     {
         cout << -1;
